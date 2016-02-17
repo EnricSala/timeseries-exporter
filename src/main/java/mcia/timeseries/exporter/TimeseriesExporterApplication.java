c73ffaf6dc4cbdd9bce9df7aa4f9ca956f624823
Enric Sala <enric.sala@mcia.upc.edu>
2016-02-17 09:43:30 +0100

    Add initial project structure

---
+++ src/main/java/mcia/timeseries/exporter/TimeseriesExporterApplication.java
@@ -0,0 +1,13 @@
+package mcia.timeseries.exporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeseriesExporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeseriesExporterApplication.class, args);
+	}
+
+}
